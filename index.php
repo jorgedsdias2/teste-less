a28493763dd4de767daef0c8cbac8a3044350295
Jorge Dias <jdsdias@yahoo.com.br>
2013-06-10 22:54:02 -0300

    Subindo primeira versão
    Iniciando projeto com Less

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet/less" type="text/css" href="styles.css" />
+	<script src="less.min.js" type="text/javascript"></script>
+</head>
+<body>
+	<div class="botao">
+		Botão com gradiente
+	</div>
+</body>
+</html>
