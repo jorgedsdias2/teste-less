194b20f91eb79a796dfdcfb44707f094a493a608
Jorge Dias <jdsdias@yahoo.com.br>
2013-07-14 21:40:43 -0300

    Testes Cookie
    Fiz alguns testes com JQuery Cookie

--- index.php
+++ index.php
@@ -4,7 +4,22 @@
 	<meta charset="utf-8">
 	<title></title>
 	<link rel="stylesheet/less" type="text/css" href="styles.less" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="less.min.js" type="text/javascript"></script>
+	<script src="jquery.cookie.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$.cookie("teste", "teste");
+			$.cookie('the_cookie', 'the_value', { expires: 7 });
+
+			alert($.cookie("the_cookie"));
+
+			$(".botao").click(function() {
+				$.removeCookie("the_cookie");
+				alert("the_cookie foi removido");
+			});
+		});
+	</script>
 </head>
 <body>
 	<div class="botao">
